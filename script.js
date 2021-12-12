d9e04a73acf55c6f952ba4ca7c1cc345155e7bf3
Gabriel Carino <gabejcarino@gmail.com>
2021-12-11 16:19:27 -0800

    add p containing script and its contents

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const h3 = document.createElement("h3");
-h3.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h3);
+const body = document.body;
+const div = body.children[1];
+const main = div.children[0];
+const p = main.children[3];
+const a = document.createElement("a");
+const linkText = document.createTextNode("the Flatiron School");
+a.appendChild(linkText);
+a.href = "https://flatironschool.com/";
+p.appendChild(a);
